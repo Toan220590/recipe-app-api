e65edd7f3a105254ddbeea9737feb3c758c45b34
Toan <quynhon.iot@gmail.com>
2023-04-11 09:26:41 +0700

    chinh sua V1

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+from app import cacl
+
+
+class calcTest(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = cacl.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = cacl.subtract(10, 15)
+        self.assertEqual(res, -5)
